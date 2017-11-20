d43aef071ee6205e960dd993787a741dd617146f
HimanshuP90 <himanshup895@gmail.com>
2017-11-20 19:10:21 +0530

    code added in shared folder

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,41 +1,9 @@
 import express from 'express';
-import Validator from 'validator';
-import isEmpty from 'lodash/isEmpty';
+import validateInput from '../shared/validations/signup';
+
 
 let router  = express.Router();
 
-function validateInput(data) {
-	let errors = {};
-
-	if (Validator.isNull(data.username)) {
-		errors.username = 'Username is required';
-	}
-	
-	if (!Validator.isEmail(data.email)){
-		errors.email = 'Email is required';
-	}
-
-	if (Validator.isNull(data.password)) {
-		errors.password = 'Password is required';
-	}
-
-	if (Validator.isNull(data.passwordConfirmation)) {
-		errors.passwordConfirmation = 'This field is required';
-	}
-
-	if (!Validator.equals(data.password, data.passwordConfirmation)) {
-		errors.passwordConfirmation = 'Password must match';
-	}
-
-	if (Validator.isNull(data.timeZone)) {
-		errors.timeZone = 'This filed is required';
-	}	
-
-	return {
-		errors,
-		isValid: isEmpty(errors)
-	}
-}
 
 router.post('/', (req,res) => {
 
