56baa247adb6a2c3f7cf1bc071a5d9fabaea8d65
HimanshuP90 <himanshup895@gmail.com>
2017-11-25 10:09:16 +0530

    singup credintals saved in databse with auth

--- client/routes.js
+++ client/routes.js
@@ -4,10 +4,16 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import Greetings from './components/Greetings';
 import SignupPage from './components/Signup/SignupPage';
+import LoginPage from './components/login/LoginPage';
+import NewEventPage from './components/events/NewEventPage';
+
+import requireAuth from './utils/requireAuth';
 
 export default (
-	<Route path="/" component={App} >
-		<IndexRoute component={Greetings} />
-		<Route path="signup" component={SignupPage} />
-	</Route>
+  <Route path="/" component={App}>
+    <IndexRoute component={Greetings} />
+    <Route path="signup" component={SignupPage} />
+    <Route path="login" component={LoginPage} />
+    <Route path="new-event" component={requireAuth(NewEventPage)} />
+  </Route>
 )
