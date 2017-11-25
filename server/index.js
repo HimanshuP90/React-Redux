71dfe9951beb08352e3cab7b99c3c35a29d51ab7
HimanshuP90 <himanshup895@gmail.com>
2017-11-25 10:11:06 +0530

    Added validations

--- server/index.js
+++ server/index.js
@@ -8,12 +8,16 @@ import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpackConfig from '../webpack.config.dev';
 
 import users from './routes/users';
+import auth from './routes/auth';
+import events from './routes/events';
 
 let app = express();
 
 app.use(bodyParser.json());
 
 app.use('/api/users', users);
+app.use('/api/auth', auth);
+app.use('/api/events', events);
 
 const compiler = webpack(webpackConfig);
 
@@ -28,4 +32,4 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
 });
 
-app.listen(3000, () => console.log('Running on localhost: 3000'));
+app.listen(3000, () => console.log('Running on localhost:3000'));
